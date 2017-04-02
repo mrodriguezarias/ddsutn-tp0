fce092bcc836785f9824c7cd614eb7c84aa4f80e
FrancoAiello <francoaiello@live.com>
2017-04-02 12:23:07 -0300

    Jugando con arena - agregado de màs componentes

--- src/main/java/ddsutn/tp0/AppView.java
+++ src/main/java/ddsutn/tp0/AppView.java
@@ -3,6 +3,9 @@ package ddsutn.tp0;
 import org.uqbar.arena.layout.VerticalLayout;
 import org.uqbar.arena.widgets.Label;
 import org.uqbar.arena.widgets.Panel;
+import org.uqbar.arena.widgets.Button;
+import org.uqbar.arena.widgets.Control;
+import org.uqbar.arena.widgets.TextBox;
 import org.uqbar.arena.windows.MainWindow;
 
 @SuppressWarnings("serial")
@@ -17,9 +20,29 @@ public class AppView extends MainWindow<AppViewModel> {
 		setTitle("Student Grades");
 		mainPanel.setLayout(new VerticalLayout());
 		new Label(mainPanel).setText(">>>>>>> Bonjour le monde ! <<<<<<<");
+		
+		new Label(mainPanel).setText("Ingresar token de alumno:");
+		Control nombre = new TextBox(mainPanel).setWidth(200);
+		
+		new Button(mainPanel).setCaption("Buscar").onClick(this::searchStudent).setAsDefault();
 	}
+	
+//	@Override
+//	public void addActions(Panel mainPanel){
+//	
+//		new Label(mainPanel)
+//		.setText("Ingresar token de alumno:");
+//		
+//		Control nombre = new TextBox(mainPanel);
+//		nombre.setWidth(200);
+//	}
 	
 	public static void main(String[] args) {
 		new AppView().startApplication();
 	}
+	
+	public void searchStudent(){
+		
+	}
+	
 }
